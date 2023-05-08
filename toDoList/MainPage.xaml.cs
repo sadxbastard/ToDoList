f24ce6151c5f0a684289503ec1ec016e4823e162
SADXBVSTVRD <SADXBVSTVRD@DESKTOP-1LOL4LG>
2023-05-09 04:33:48 +0500

    Начало модели и представления модели

--- toDoList/MainPage.xaml.cs
+++ toDoList/MainPage.xaml.cs
@@ -1,18 +1,34 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace toDoList;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
 	}
+    public void ClickOnAddButton(object sender, EventArgs e)
+    {
+        inputText.Text = string.Empty;
+    }
 }
-class PersonViewModel : INotifyPropertyChanged
+
+public class ColorItem
+{
+    public string Name { get; set; }
+    public Color Value { get; set; }
+    public override string ToString()
+    {
+        return Name;
+    }
+
+}
+class ViewModel : INotifyPropertyChanged
 {
 	private string _surname;
 	public string Surname
@@ -27,9 +43,21 @@ class PersonViewModel : INotifyPropertyChanged
 	}
     public event PropertyChangedEventHandler PropertyChanged;
 
-	protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-	{
-		PropertyChanged ?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-	}
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public ObservableCollection<ColorItem> MyColors { get; set; } = new ObservableCollection<ColorItem>
+    {
+        new ColorItem { Name = "Без категории", Value = Colors.White },
+		new ColorItem { Name = "Черный", Value = Colors.Black },
+        new ColorItem { Name = "Синий", Value = Colors.Blue },
+        new ColorItem { Name = "Красный", Value = Colors.Red },
+        new ColorItem { Name = "Зеленый", Value = Colors.Green },
+    };
+    public ICommand AddCommand { get; }
+    public ICommand DeleteCommand { get; }
+    public ICommand DoneCommand { get; }
 }
 
